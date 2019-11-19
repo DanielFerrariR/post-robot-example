13874d7e6a6b47fd7eb0e8587a34d9b515b8a1cc
Daniel Ferrari <danielferrari@live.com>
2019-11-19 12:55:32 -0300

    refactor

--- parent/src/App.js
+++ parent/src/App.js
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import postRobot from "post-robot";
 
 const App = () => {
   const [childWindow, setChildWindow] = useState("");
 
-  useEffect(() => {
-    postRobot.on("foo", event => {
-      var el = document.getElementById("child-message");
-      el.innerHTML += "\n" + event.data.message;
-      return {
-        message: event.data.message
-      };
-    });
-  }, []);
-
   const openWindow = () => {
-    const newWindow = window.open("http://localhost:3000/");
+    const newWindow = window.open("http://localhost:3000");
+
+    postRobot.on(
+      "foo",
+      { window: newWindow, domain: "http://localhost:3000" },
+      event => {
+        var el = document.getElementById("child-message");
+        el.innerHTML += "\n" + event.data.message;
+        return {
+          message: event.data.message
+        };
+      }
+    );
+
     setChildWindow(newWindow);
   };
 
   const sendMessage = async () => {
     try {
-      const event = await postRobot.send(childWindow, "bar", {
-        message: Math.random().toString()
-      });
+      const event = await postRobot.send(
+        childWindow,
+        "bar",
+        {
+          message: Math.random().toString()
+        },
+        { domain: "http://localhost:3000" }
+      );
       const el = document.getElementById("parent-message");
       el.innerHTML += "\n" + event.data.message;
     } catch (error) {
