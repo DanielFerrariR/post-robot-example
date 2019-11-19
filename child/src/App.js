2592177e74c49f1e8bf1c2e51a4c049636c13b83
Daniel Ferrari <danielferrari@live.com>
2019-11-19 12:02:17 -0300

    added parent child communication

--- child/src/App.js
+++ child/src/App.js
@@ -1,22 +1,34 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import postRobot from "post-robot";
 
-function App() {
-  function sendMessage() {
-    postRobot
-      .send(window.opener, "foo", { message: Math.random().toString() })
-      .then(function(event) {
-        var el = document.getElementById("messages");
-        el.innerHTML += "\n" + event.data.message;
+const App = () => {
+  useEffect(() => {
+    postRobot.on("bar", event => {
+      var el = document.getElementById("parent-message");
+      el.innerHTML += "\n" + event.data.message;
+      return {
+        message: event.data.message
+      };
+    });
+  }, []);
+
+  const sendMessage = async () => {
+    try {
+      const event = await postRobot.send(window.opener, "foo", {
+        message: Math.random().toString()
       });
-  }
+      const el = document.getElementById("child-message");
+      el.innerHTML += "\n" + event.data.message;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      <button onClick={() => sendMessage()}>post</button>
-      <div id="messages"></div>
+      <button onClick={sendMessage}>post</button>
+      <div id="parent-message">Parent Message:</div>
+      <div id="child-message">Child Message:</div>
     </div>
   );
 }
